0c29d0f01d1ab4bf4aa22887054a5920dd43182b
eric <deadgay0625@gmail.com>
2025-02-27 15:39:20 +0800

    添加 Kubernetes 服务和 Kustomization 配置，更新 .gitignore 以包含环境变量文件路径

--- incubation/kubernetes/infra/bootstrap.sh
+++ incubation/kubernetes/infra/bootstrap.sh
@@ -34,6 +34,7 @@ NATS_SERVER_PASSWORD=$(rnd_password)
 NATS_CLIENT_PASSWORD=$(rnd_password)
 SYSTEM_MANGER_POSTGRES_PASSWORD=$(rnd_password)
 SYSTEM_MANGER_KEYCLOAK_SECRET=$(rnd_password)
+SYSTEM_MANGER_WEB_NEXT_AUTH_SECRET=$(rnd_password)
 # keycloak
 if [[ -f postgres/overlays/keycloak/postgres.env ]]; then
     echo "keycloak postgres.env already exists"
@@ -99,6 +100,16 @@ authorization {
 EOF
 fi
 
+if [[ -f nats.env ]]; then
+    echo "nats.env already exists"
+else
+    cat <<EOF > nats.env
+NATS_ADMIN_PASSWORD=${NATS_ADMIN_PASSWORD}
+NATS_SERVER_PASSWORD=${NATS_SERVER_PASSWORD}
+NATS_CLIENT_PASSWORD=${NATS_CLIENT_PASSWORD}
+EOF
+fi
+
 # system-manager
 if [[ -f postgres/overlays/system-manager/postgres.env ]]; then
     echo "system-manager postgres.env already exists"
@@ -139,6 +150,50 @@ kubectl apply -k keycloak/overlays/prod
 kubectl create ns prod-nats
 kubectl apply -k nats/overlays/prod
 
+# 等待keycloak容器状态正常
+while [[ $(kubectl get pods -n prod-keycloak | grep keycloak | awk '{print $3}') != "Running" ]]; do
+    sleep 5
+    echo "waiting for keycloak pod to be ready"
+done
+
 kubectl create ns prod-system-manager
 kubectl apply -k postgres/overlays/system-manager
 kubectl apply -k system-manager/overlays/prod
+
+# 等待system-manager容器状态正常
+while [[ $(kubectl get pods -n prod-system-manager | grep system-manager | awk '{print $3}') != "Running" ]]; do
+    sleep 5
+    echo "waiting for system-manager pod to be ready"
+done
+
+# 刷新lite和opspilot的token
+if [[ -f keycloak_token.env ]]; then
+    echo "keycloak_token.env already exists"
+    source keycloak_token.env
+else
+    LITE_TOKEN_RAW=$(kubectl exec -i -n prod-keycloak $(kubectl get pods -n prod-keycloak -l app=keycloak -o=jsonpath='{.items[*].metadata.name}') -- /opt/keycloak/bin/refresh_token.sh lite 2>&1)
+    LITE_TOKEN=$(echo $LITE_TOKEN_RAW | awk -F'"value" *: *"' '{print $2}' | awk -F'"' '{print $1}')
+    OPSPILIT_TOKEN_RAW=$(kubectl exec -i -n prod-keycloak $(kubectl get pods -n prod-keycloak -l app=keycloak -o=jsonpath='{.items[*].metadata.name}') -- /opt/keycloak/bin/refresh_token.sh opspilot 2>&1)
+    OPSPILIT_TOKEN=$(echo $OPSPILIT_TOKEN_RAW | awk -F'"value" *: *"' '{print $2}' | awk -F'"' '{print $1}')
+    cat <<EOF > keycloak_token.env
+LITE_TOKEN=${LITE_TOKEN}
+OPSPILIT_TOKEN=${OPSPILIT_TOKEN}
+DOMAIN=${DOMAIN}
+EOF
+fi
+
+# 启动system-manager-web
+if [[ -f system-manager-web/overlays/prod/system-manager-web.env ]]; then
+    echo "system-manager-web.env already exists"
+else
+    cat <<EOF > system-manager-web/overlays/prod/system-manager-web.env
+KEYCLOAK_CLIENT_ID=lite
+KEYCLOAK_CLIENT_SECRET=${LITE_TOKEN}
+KEYCLOAK_ISSUER=http://${DOMAIN}:38080/realms/lite
+NEXTAUTH_URL=http://${DOMAIN}:38081
+NEXTAUTH_SECRET=${SYSTEM_MANGER_WEB_NEXT_AUTH_SECRET}
+NEXTAPI_URL=http://system-manager:8000
+EOF
+fi
+
+kubectl apply -k system-manager-web/overlays/prod
